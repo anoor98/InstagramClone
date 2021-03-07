88c8a654a6b21950dd93450507fc24c584b942e0
anoor98 <aliemanoor@gmail.com>
2021-03-07 17:04:05 -0500

    Project update. Project complete and functional

--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -3,6 +3,7 @@ package com.example.instagramclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
@@ -11,10 +12,16 @@ public class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        //register your parse models
+        ParseObject.registerSubclass(Post.class);
+
         //Register Parse Models
+        //set applicationID, and server server based on the values in the Back4app settings
+        //clientKey is not needed unless explicitly configured
+        //any network interceptors must be added with the configuration builder given this
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("gfHEf6qk0j002c8VpCF0dbayZgZGRyr7W1huPpwX")
-                .clientKey("odEsKGrD1dfDI5ggGInDcXClL83XCrdYV2xu5RfX")
+                .applicationId("gfHEf6qk0j002c8VpCF0dbayZgZGRyr7W1huPpwX") //should correspond to APP_ID env variable
+                .clientKey("odEsKGrD1dfDI5ggGInDcXClL83XCrdYV2xu5RfX") //set explicitly unless clientKey is expl
                 .server("https://parseapi.back4app.com")
                 .build()
         );
---
+++ app/src/main/java/com/example/instagramclone/Post.java
@@ -0,0 +1,27 @@
+package com.example.instagramclone;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Post")
+public class Post extends ParseObject {
+
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_USER = "user";
+
+    //Post Descriptions
+    public String getDescription() { return getString(KEY_DESCRIPTION); }
+    public void setDescription(String description) { put(KEY_DESCRIPTION, description); }
+
+    //Post Images
+    public ParseFile getImage() { return getParseFile(KEY_IMAGE); }
+    public void setImage(ParseFile parseFile) { put(KEY_IMAGE, parseFile); }
+
+    //Post User
+    public ParseUser getUser() { return getParseUser(KEY_USER); }
+    public void setUser(ParseUser parseUser) { put(KEY_USER, parseUser); }
+
+}
